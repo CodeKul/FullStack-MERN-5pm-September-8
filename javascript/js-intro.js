c8cd084ddbe9e49c6c7cc58b557e33847bf7bcf9
Sanket <sanketbhosale.codekul@gmail.com>
2021-10-14 18:06:45 +0530

    JS-Intro

--- javascript/js-intro.js
+++ javascript/js-intro.js
@@ -1,14 +1,69 @@
-alert("welcome to js")
-
+//alert("welcome to js")
 
 // data-type var-name;
 // var-name = 10;
 
+// var keyword variable are global scope variable as well as function scope variable
+
+// let keyword variable are block scope variable as well as function scope variable
+
+// function scope variable can only accesses within the function block and not outside it
 
 // var, let and const
+PI=3.14
+console.log(PI)
 
-var num1 = 10;
+// 1.declaration 
+// 2.initialization 
+// 3.using the variable
+
+
+// write a code for calculating total marks, percentage and grade of a student
+
+// https://www.youtube.com/watch?v=Fnlnw8uY6jo
+// https://www.youtube.com/watch?v=BNC6slYCj50
+// https://www.youtube.com/watch?v=lW_erSjyMeM
+
+
+const num1 = 10;
+
+console.log(num1)
+
+// num1=159;
+
+console.log(num1)
 
 let num2 = 20;
 
-const PI = 3.14
+
+
+var PI;   //declared and initialized
+
+ PI=1000     //initialized
+
+
+ var PI = 2000; //declaring and initialized
+
+console.log(PI)
+
+// let fname;
+//     fname="sanket";
+
+//console.log(fname);
+
+function displayMessage() {
+  var fname;
+  fname = "sanket";
+  console.log(fname);
+}
+
+
+if(true){
+    let num4;
+    num4=253;
+   
+}
+
+console.log(num4)
+
+displayMessage();
