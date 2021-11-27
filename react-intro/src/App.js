2c809d8ba958d3d29c57b73ed684ba76dbfe57f5
sanketcodekul <sanketbhosale.codekul@gmail.com>
2021-11-27 12:16:14 +0530

    api consumption revision

---
+++ react-intro/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+// React Dom, Virtual Dom, Working of React Application
+// what is the difference between real dom and virtual dom
+
+function App() {
+  return (
+    <div className="App">
+      Codekull... 
+      <h1>Javascript is BEST...</h1>
+      {/* <h1>App component</h1> */}
+         </div>
+  );
+}
+
+export default App;
