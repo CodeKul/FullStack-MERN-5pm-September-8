26a9a64e56c2e8c92e2c6ab33da874178f866e3a
sanketcodekul <sanketbhosale.codekul@gmail.com>
2022-01-03 18:40:38 +0530

    Redux intro

---
+++ react-redux-intro/react-redux-intro/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+// https://www.youtube.com/watch?v=9boMnm5X9ak&list=PLC3y8-rFHvwheJHvseC3I0HuYI2f46oAK 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-intro/react-redux-intro/src/BankOperation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BankOperation() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
